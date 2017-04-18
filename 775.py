367379ea671e936f93a76fccc2c16cf8beafec6d
Nacho Yegro <nachoyegro@gmail.com>
2017-04-18 08:50:22 -0300

    868 accepted, 775 time limit exceeded

--- 775.py
+++ 775.py
@@ -4,7 +4,10 @@ ENCONTRADO = False
 CAMINO = []
 
 def read_line():
-    line = next(sys.stdin).strip()
+    try:
+        line = next(sys.stdin).strip()
+    except:
+        return ''
     while(len(line) == 0):
         line = next(sys.stdin).strip()
     return line
@@ -27,6 +30,7 @@ def backtracking(grafo, visitables, nodo_actual):
             CAMINO.append(nodo)
             #Hago backtracking sobre los nodos restantes
             backtracking(grafo, grafo[nodo], nodo)
+            #O(1)
             if esHamiltoniano(nodo):
                 ENCONTRADO = True
                 print(' '.join([str(NODO_INICIAL)] + [str(v) for v in CAMINO]))
@@ -44,27 +48,31 @@ if __name__ == "__main__":
         #Me traigo la primer linea, que es la cantidad de nodos
         nodos = read_line()
         arista = ''
-        TOTAL_NODOS = int(nodos)
-        while arista != '%':
-            #Traigo sus nodos
-            if arista:
-                #Los pongo en la matriz
-                a, b = arista.split(" ")
-                if a in G:
-                    G[a].append(b)
-                else:
-                    G[a] = [b]
-                if b in G:
-                    G[b].append(a)
-                else:
-                    G[b] = [a]
-                vertices.append(a)
-                vertices.append(b)
-            arista = read_line()
-        NODO_INICIAL = vertices[0]
-        for nodo in vertices:
-            backtracking(G, G[nodo], nodo)
-            if ENCONTRADO:
-                break
-        if not ENCONTRADO:
-            print('N')
+        if nodos:
+            TOTAL_NODOS = int(nodos)
+            while arista != '%':
+                #Traigo sus nodos
+                if arista:
+                    #Los pongo en la matriz
+                    a, b = arista.split(" ")
+                    if a in G:
+                        G[a].append(b)
+                    else:
+                        G[a] = [b]
+                    if b in G:
+                        G[b].append(a)
+                    else:
+                        G[b] = [a]
+                    vertices.append(a)
+                    vertices.append(b)
+                arista = read_line()
+            if vertices:
+                NODO_INICIAL = vertices[0]
+                for nodo in vertices:
+                    if ENCONTRADO:
+                        break
+                    backtracking(G, G[nodo], nodo)
+            if not ENCONTRADO:
+                print('N')
+        else:
+            break
--- 868.py
+++ 868.py
@@ -1,19 +1,17 @@
 import sys
 
-ENCONTRADO = False
 CAMINO = []
 MAX = 1
 ULTIMO = 1
 ULTIMA_FILA = 0
 ULTIMA_COLUMNA = 0
-G = { 0:{0:2, 1:3, 2:1, 3:4},
-      1:{0:3, 1:2, 2:1, 3:3},
-      2:{0:2, 1:1, 2:2, 3:4},
-      3:{0:3, 1:1, 2:2, 3:4},
-      4:{0:1, 1:2, 2:3, 3:4},}
+RESULTADOS = []
+MAZES = []
 
 def read_line():
     line = next(sys.stdin).strip()
+    if not line:
+        return ""
     while(len(line) == 0):
         line = next(sys.stdin).strip()
     return line
@@ -21,20 +19,25 @@ def read_line():
 def posibles_movimientos(ultimo, x, y, visitados):
     global ULTIMA_FILA, ULTIMA_COLUMNA
     posibles = []
+    #Hago esto porque sino, estando en el final, me da como posible movimiento el de al lado
+    if x == ULTIMA_FILA:
+        return posibles
     if x+1 <= ULTIMA_FILA and G[x+1][y] == ultimo and not (x+1, y) in visitados:
         posibles.append((x+1, y))
-    if x-1 >= 0 and G[x-1][y] == ultimo and not (x-1, y) in visitados:
+    if x-1 >= 1 and G[x-1][y] == ultimo and not (x-1, y) in visitados:
         posibles.append((x-1, y))
     if y+1 <= ULTIMA_COLUMNA and G[x][y+1] == ultimo and not (x, y+1) in visitados:
         posibles.append((x, y+1))
-    if y-1 >= 0 and G[x][y-1] == ultimo and not (x, y-1) in visitados:
+    if y-1 >= 1 and G[x][y-1] == ultimo and not (x, y-1) in visitados:
         posibles.append((x, y-1))
     return posibles
 
 
 def backtracking(ultimo, x, y, max, visitados=[]):
-    if y == ULTIMA_FILA:
-        print((x, y))
+    global RESULTADOS
+    if x == ULTIMA_FILA and (INICIO[0], INICIO[1], x, y) not in RESULTADOS:
+        RESULTADOS.append((INICIO[0], INICIO[1], x, y))
+        return
     """
         * Aca determino si tengo que arrancar el contador de nuevo
            o si sigo aumentandolo
@@ -50,13 +53,42 @@ def backtracking(ultimo, x, y, max, visitados=[]):
         ultimo += 1
     for i, j in posibles_movimientos(ultimo, x, y, visitados):
         visitados.append((i, j))
-        backtracking(ultimo, i, j, max)
+        backtracking(ultimo, i, j, max, visitados)
         visitados.remove((i, j))
-    return
 
 if __name__ == "__main__":
-    ULTIMA_FILA = 4
-    ULTIMA_COLUMNA = 3
-    for i in G[0]:
-        if G[0][i] == 1:
-            backtracking(1, 0, i, 1)
+    cases = int(read_line())
+    for i in range(0, cases):
+        G = {}
+        blank = read_line()#blank
+        filas, columnas = map(int, read_line().split(" "))
+        RESULTADOS = []
+        ULTIMA_FILA = filas
+        ULTIMA_COLUMNA = columnas
+        for i in range(1, filas+1):
+            fila = read_line().split(" ")
+            G[i] = {}
+            for j in range(1, columnas+1):
+                G[i][j] = int(fila[j-1])
+        for i in G[1]:
+            if G[1][i] == 1:
+                INICIO = (1, i)
+                backtracking(1, 1, i, 1) #ultimo, x, y, maximo
+        minimo = ()
+        for ix, iy, fx, fy in RESULTADOS:
+            if not minimo:
+                minimo = (ix, iy, fx, fy)
+            else:
+                #Si lexicograficamente el inicio actual esta mas a la izquierda que el minimo
+                # o lexicograficamente el inicio es igual, pero el final esta mas a la izquierda
+                if iy < minimo[1] or (iy == minimo[1] and fy < minimo[3]) and G[fx][fy] >= G[minimo[2]][minimo[3]]:
+                    #Queda como minimo actual
+                    minimo = (ix, iy, fx, fy)
+        MAZES.append(minimo)
+    #Si no hago esto, me da presentation error
+    for maze in MAZES[:-1]:
+        print(maze[0], maze[1])
+        print(maze[2], maze[3])
+        print("")
+    print(MAZES[-1][0], MAZES[-1][1])
+    print(MAZES[-1][2], MAZES[-1][3])
