08b46d34248832a55dae27575d612016eb810f7b
Nacho Yegro <juan.yegro@unq.edu.ar>
2017-05-08 15:01:22 -0300

    11003 ofi

--- 11003.py
+++ 11003.py
@@ -1,9 +1,14 @@
 BOXES = [(19, 15), (7, 13), (5, 7), (6, 8), (1, 2)]
+mem = {}
 
 def backtracking(n, w):
-    if n == 0:
-        return 0
-    return max(backtracking(n-1) + weight(n), backtracking(n-1))
+	#Si llegue al final, corto
+	if n == len(BOXES):
+		return 0
+	#Si el peso acumulado es mayor que lo que soporta la caja actual, corto
+	if load(n) < w:
+		return -1
+	return max(1 + backtracking(n+1, w+weight(n)), backtracking(n+1, w))
 
 def weight(n):
     #Devuelvo el primer elemento del par
@@ -12,3 +17,8 @@ def weight(n):
 def load(n):
     #Devuelvo el segundo elemento del par
     return BOXES[n][1]
+
+if __name__ == "__main__":
+	mem = {0:{}, 1:{}, 2:{}, 3:{}, 4:{}}
+	for i in range(0, len(BOXES)):
+		print(backtracking(i, 0))
