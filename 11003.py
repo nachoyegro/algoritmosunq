c5ba5413eaa26399960f77e1e33b87bf50049cd4
Nacho Yegro <nachoyegro@gmail.com>
2017-05-08 09:43:55 -0300

    Primer approach

--- 11003.py
+++ 11003.py
@@ -1,9 +1,14 @@
 BOXES = [(19, 15), (7, 13), (5, 7), (6, 8), (1, 2)]
+CANTIDAD = 0
 
-def backtracking(n, w):
+def backtracking(n, acumulado):
+    global CANTIDAD
     if n == 0:
-        return 0
-    return max(backtracking(n-1) + weight(n), backtracking(n-1))
+        return cajas
+    if acumulado > load(n):
+        return -1
+    else:
+        return max(backtracking(n-1, acumulado+weight(n), cajas + 1), backtracking(n-1, acumulado, cajas))
 
 def weight(n):
     #Devuelvo el primer elemento del par
@@ -12,3 +17,7 @@ def weight(n):
 def load(n):
     #Devuelvo el segundo elemento del par
     return BOXES[n][1]
+
+if __name__ == "__main__":
+    for box in BOXES:
+    backtracking(len(BOXES))
