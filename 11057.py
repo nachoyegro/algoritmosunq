4f9774d77abae53121087f1f5e2bca1a5de6842c
Nacho Yegro <nachoyegro@gmail.com>
2017-05-30 09:02:29 -0300

    11057 accepted

--- 11057.py
+++ 11057.py
@@ -13,52 +13,36 @@ def read_line():
     except:
         return None
 
-def libros():
-    global LIBROS, PRECIO, CANT
-    #Ordeno los libros de menor a mayor
-    libros = sorted(LIBROS)
-    #Inicializo los libros que van a ser la solucion
-    a = 0
-    b = 10001
-    #Recorro
-    for libro in range(0, CANT):
-        #tmp es el precio actual cuando le resto el libro actual
-        tmp = PRECIO - libros[libro]
-        #Si el precio del libro actual es mayor a lo que me queda, corto
-        if tmp < libros[libro]:
-            continue
-        #Si el precio del libro actual es igual a lo que me queda y no hay repetidos, corto
-        #Obs: Esto lo tuve que hacer porque en un caso borde donde el numero del medio es igual
-        #     a lo que me queda, me lo repetia (por ejemplo, si el precio total es 160 y el libro del medio da 80,
-        #     me ponia dos veces 80)
-        if tmp == libros[libro] and libro < (CANT - 1) and libros[libro] != libros[libro+1]:
-            continue
-        #Inicializo izquierda y derecha
-        izq = 0
-        der = CANT - 1
-        while izq < der:
-            #Inicializo el medio
-            mid = (izq+der)//2
-            if libros[mid] < tmp:
-                izq = mid+1
-            else:
-                der = mid
-        #Despues de iterar me fijo si encontre una solucion
-        if libros[der] == tmp:
-            if tmp - libros[libro] < b - a:
-                a = libros[libro]
-                b = tmp
-    print("Peter should buy books whose prices are %s and %s." % (min(a, b),max(a, b)))
-    print("")
+def libros(m, n):
+    #Ordeno e inicializo
+    L.sort()
+    i = 0
+    j = n - 1
+    a,b = 0, 0
+    #Recorro hasta que llego al mismo indice
+    while i < j:
+        #Si la suma de los extremos actuales es menor, me muevo hacia la derecha
+        if(L[i] + L[j] < m):
+            i+=1
+        #Si llegué, pongo esta como solución parcial y me muevo hacia el centro
+        elif L[i] + L[j] == m:
+            a = i
+            b = j
+            i+=1
+            j-=1
+        #Si es mayor, entonces me muevo hacia la izquierda
+        else:
+            j-=1
+    print('Peter should buy books whose prices are %d and %d.' % (L[a], L[b]))
+    print('')
 
 if __name__ == "__main__":
     termine = False
-    while not termine:
-        line = read_line()
-        if not line:
-            termine = True
-        else:
-            CANT = int(line)
-            LIBROS = map(int, read_line().split(" "))
+    while True:
+        try:
+            CANT = int(read_line())
+            L = list(map(int, read_line().split(" ")))
             PRECIO = int(read_line())
-            libros()
+            libros(PRECIO, CANT)
+        except:
+            break
