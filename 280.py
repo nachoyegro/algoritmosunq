996dd8ff9b7a939c4d051fa36aed76ec0bc89b6a
Nacho Yegro <juan.yegro@unq.edu.ar>
2017-06-06 14:48:02 -0300

    280 con time limit

--- 280.py
+++ 280.py
@@ -1,23 +1,41 @@
-GRAPH = {0:[1, 4], 1:[0, 2, 4], 2:[1, 3, 4], 3:[2, 4], 4: [0, 1, 2, 3]}
-VISITED = {0: 0, 1:0, 2:0, 3:0, 4:0}
+import sys
+GRAPH = {}
+VISITED = {}
 
+def read_line():
+    return next(sys.stdin).strip()
 
 def dfs(start):
     global GRAPH, VISITED
-    #Inicializo el stack
-    stack = [start]
-    #Mientras me quedan cosas
-    while stack:
-        #Traigo el proximo elemento
-        vertex = stack.pop()
-        #Recorro sus vecinos
-        for ngb in GRAPH[vertex]:
-            #Si no fue visitado, lo agrego al stack
-            if not VISITED[ngb]:
-                stack.append(ngb)
-        #Lo pongo como visitado
-        VISITED[vertex] = 1
-    return VISITED
+    #Recorro sus vecinos
+    for ngb in GRAPH[start]:
+        #Si no fue visitado, lo agrego al stack
+        if not VISITED[ngb]:
+            #Lo pongo como visitado
+            VISITED[ngb] = 1
+            dfs(ngb)
 
 if __name__ == "__main__":
-    print(dfs(0))
+    nodes = read_line()
+    #Si no es la ultima linea, sigo
+    while nodes != '0':
+        GRAPH = {}
+        VISITED = {}
+        nodes = int(nodes)
+        #Genero el grafo
+        GRAPH = {i: [] for i in range(1, nodes+1)}
+        edges = [int(x) for x in read_line().split()][:-1]
+        while edges:
+            #Genero las listas de adyacencias
+            start_edge = edges[0]
+            GRAPH[start_edge] = edges[1:]
+            edges = [int(x) for x in read_line().split()][:-1]
+        test = [int(x) for x in read_line().split()]
+        cant = test[0]
+        for start in test[1:]:
+            VISITED = {i: 0 for i in range(1, nodes+1)}
+            dfs(start)
+            not_visited = [str(i) for i in VISITED.keys() if not VISITED[i]]
+            cant = len(not_visited)
+            print(" ".join([str(cant)] + not_visited))
+        nodes = read_line()
---
+++ dfs.py
@@ -0,0 +1,23 @@
+GRAPH = {0:[1, 4], 1:[0, 2, 4], 2:[1, 3, 4], 3:[2, 4], 4: [0, 1, 2, 3]}
+VISITED = {0: 0, 1:0, 2:0, 3:0, 4:0}
+
+
+def dfs(start):
+    global GRAPH, VISITED
+    #Inicializo el stack
+    stack = [start]
+    #Mientras me quedan cosas
+    while stack:
+        #Traigo el proximo elemento
+        vertex = stack.pop()
+        #Recorro sus vecinos
+        for ngb in GRAPH[vertex]:
+            #Si no fue visitado, lo agrego al stack
+            if not VISITED[ngb]:
+                stack.append(ngb)
+        #Lo pongo como visitado
+        VISITED[vertex] = 1
+    return VISITED
+
+if __name__ == "__main__":
+    pass
