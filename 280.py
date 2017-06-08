5d5f93f5c30f2d95e08ef8f99a01bd4bc9b392d7
Nacho Yegro <juan.yegro@unq.edu.ar>
2017-06-08 12:54:14 -0300

    Mejoras al 280, pero sigue con time limit

--- 280.py
+++ 280.py
@@ -1,41 +1,58 @@
 import sys
-GRAPH = {}
-VISITED = {}
+GRAPH = []
+VISITED = []
 
 def read_line():
     return next(sys.stdin).strip()
 
 def dfs(start):
     global GRAPH, VISITED
-    #Recorro sus vecinos
-    for ngb in GRAPH[start]:
-        #Si no fue visitado, lo agrego al stack
-        if not VISITED[ngb]:
-            #Lo pongo como visitado
-            VISITED[ngb] = 1
-            dfs(ngb)
+    stack = [start]
+    #Mientras me quedan cosas
+    while stack:
+        #Traigo el proximo elemento
+        vertex = stack.pop()
+        #Recorro sus vecinos
+        for ngb in GRAPH[vertex]:
+            if not VISITED[ngb]:
+                #Lo pongo como visitado
+                VISITED[ngb] = 1
+                #Lo pongo en el stack
+                stack.append(ngb)
 
 if __name__ == "__main__":
+    result = ""
     nodes = read_line()
     #Si no es la ultima linea, sigo
     while nodes != '0':
         GRAPH = {}
         VISITED = {}
         nodes = int(nodes)
-        #Genero el grafo
-        GRAPH = {i: [] for i in range(1, nodes+1)}
+        #Genero la matriz
+        GRAPH = [[] for _ in range(nodes+1)]
         edges = [int(x) for x in read_line().split()][:-1]
         while edges:
             #Genero las listas de adyacencias
             start_edge = edges[0]
-            GRAPH[start_edge] = edges[1:]
+            GRAPH[start_edge] += edges[1:]
+            #Aristas sacando el ultimo 0
             edges = [int(x) for x in read_line().split()][:-1]
         test = [int(x) for x in read_line().split()]
         cant = test[0]
-        for start in test[1:]:
-            VISITED = {i: 0 for i in range(1, nodes+1)}
+        #Arranco de 1 porque no quiero mirar el 0
+        for i in range(1, len(test)):
+            start = test[i]
+            #Genero la lista de visitados en 0
+            VISITED = [0] * (nodes + 1)
+            #Hago DFS con el start actual
             dfs(start)
-            not_visited = [str(i) for i in VISITED.keys() if not VISITED[i]]
+            #Traigo la lista de no visitados
+            not_visited = [str(i) for i in range(1, len(VISITED)) if not VISITED[i]]
             cant = len(not_visited)
-            print(" ".join([str(cant)] + not_visited))
+            if result:
+                case = "\n"
+            else:
+                case = ""
+            result = case.join([result, " ".join([str(cant)] + not_visited)])
         nodes = read_line()
+    print(result)
