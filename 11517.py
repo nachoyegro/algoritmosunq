253c8e5f9e73b42b2c413ec8386ac194f0a5cb69
Nacho Yegro <juan.yegro@unq.edu.ar>
2017-06-23 15:32:26 -0300

    Mejoras al 11517

--- 11517.py
+++ 11517.py
@@ -1,7 +1,6 @@
 import sys
 
 BILLETES = []
-USADOS = 0
 PRECIO = 0
 CACHE = {}
 NOT_INIT = -1
@@ -11,26 +10,22 @@ def read_line():
     return next(sys.stdin).strip()
 
 def crear_matriz(billetes):
-    return [[NOT_INIT] * (PRECIO+1)] * (billetes+1)
+    return [[NOT_INIT] * (PRECIO+1) for _ in range(billetes+1)]
 
 def backtracking(acumulado, num):
-    global BILLETES, USADOS, PRECIO, MIN
+    global BILLETES, PRECIO
     if acumulado >= PRECIO:
         #Como tengo que minimizar el vuelto dado, y la cantidad de BILLETES
-        #Lo retorno como un par, de lo que sobra para el vendedor y la cantidad de billetes que use
-        return (acumulado, USADOS)
+        #Lo retorno como un par, de lo que sobra para el vendedor y 0
+        return (acumulado, 0)
     #Si ya use todos los billetes y no llegué, retorno cualquier cosa
     if num == len(BILLETES):
-        #Retorno 10.001 porque el precio no puede superar 10.000, entonces funciona como INF
-        return (INF,len(BILLETES) + 1)
-    #TODO El problema aca es que en algun momento se guarda como mejor uno que no es tan bueno como el actual,
-    # entonces ni lo considera
+        return (INF,INF)
     if CACHE[num][acumulado] == NOT_INIT:
         acumulado_usado = acumulado+BILLETES[num]
         no_usarlo = backtracking(acumulado, num+1)
-        USADOS += 1
         usarlo = backtracking(acumulado_usado, num+1)
-        USADOS -= 1
+        usarlo = (usarlo[0], usarlo[1] + 1)
         CACHE[num][acumulado] = min(usarlo, no_usarlo)
     return CACHE[num][acumulado]
 
