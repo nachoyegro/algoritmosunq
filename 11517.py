9497521791809c49eef5de3430ab4b0e5d018be6
Nacho Yegro <nachoyegro@gmail.com>
2017-06-25 11:56:35 -0300

    11517 mc

--- 11517.py
+++ 11517.py
@@ -11,26 +11,25 @@ def read_line():
     return next(sys.stdin).strip()
 
 def crear_matriz(billetes):
-    return [[NOT_INIT] * (PRECIO+1)] * (billetes+1)
+    return [[NOT_INIT for _ in range(PRECIO+1)] for _ in range(billetes+1)]
 
 def backtracking(acumulado, num):
     global BILLETES, USADOS, PRECIO, MIN
     if acumulado >= PRECIO:
         #Como tengo que minimizar el vuelto dado, y la cantidad de BILLETES
         #Lo retorno como un par, de lo que sobra para el vendedor y la cantidad de billetes que use
-        return (acumulado, USADOS)
+        return (acumulado, 0)
     #Si ya use todos los billetes y no llegué, retorno cualquier cosa
     if num == len(BILLETES):
         #Retorno 10.001 porque el precio no puede superar 10.000, entonces funciona como INF
-        return (INF,len(BILLETES) + 1)
+        return (INF, INF)
     #TODO El problema aca es que en algun momento se guarda como mejor uno que no es tan bueno como el actual,
     # entonces ni lo considera
     if CACHE[num][acumulado] == NOT_INIT:
         acumulado_usado = acumulado+BILLETES[num]
-        no_usarlo = backtracking(acumulado, num+1)
-        USADOS += 1
         usarlo = backtracking(acumulado_usado, num+1)
-        USADOS -= 1
+        usarlo = (usarlo[0], usarlo[1] + 1)
+        no_usarlo = backtracking(acumulado, num+1)
         CACHE[num][acumulado] = min(usarlo, no_usarlo)
     return CACHE[num][acumulado]
 
@@ -41,9 +40,10 @@ if __name__ == "__main__":
     for i in range(0, tests):
         PRECIO = int(read_line())
         billetes = int(read_line())
-        for i in range(0, billetes):
+        for _ in range(0, billetes):
             actual = int(read_line())
             BILLETES.append(actual)
+        #TODO: probar inicializar la cache una sola vez
         CACHE = crear_matriz(billetes)
         resultado = backtracking(0, 0)
         print(resultado[0], resultado[1])
